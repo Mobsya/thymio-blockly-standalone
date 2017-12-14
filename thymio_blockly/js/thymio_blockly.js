f2c486eb26b0db33bceb917f06a1ebeeb0e25966
Elisa Bernardoni <elisa.bernardoni@gmail.com>
2017-12-14 11:57:13 +0100

    Update thymio_blockly.js
    Cleaned up whitespaces

--- thymio_blockly/js/thymio_blockly.js
+++ thymio_blockly/js/thymio_blockly.js
@@ -3,7 +3,6 @@ goog.require('goog.net.XhrIo');
 goog.require('goog.structs.Map');
 goog.require('goog.Uri.QueryData');
 goog.require('goog.net.XhrManager');
-
 'use strict';
 var workspace = null;
 var connected = false;
@@ -37,25 +36,20 @@ function start() {
             scaleSpeed: 1.1
         },
     });
-
     //to change code in the textarea real time
     if (Blockly.Realtime.isEnabled()) {
         enableRealtimeSpecificUi();
     }
     workspace.addChangeListener(updateCode);
-
     //to load code and block from a aesl file using drag and drop
     var importExport = document.getElementById('importExport');
-
     importExport.ondragover = function() {
         this.className = 'hover';
         return false;
     };
-
     importExport.ondrop = function(e) {
         this.className = '';
         e.preventDefault();
-
         var fileToLoad = e.dataTransfer.files[0],
             fileReader = new FileReader();
         fileReader.onload = function(fileLoadedEvent) {
@@ -70,70 +64,53 @@ function start() {
                 xmlDoc.async = false;
                 xmlDoc.loadXML(textFromFileLoaded);
             }
-
             var aeslLoaded = document.getElementById('importExport');
             aeslLoaded = xmlDoc.getElementsByTagName("node")[0].childNodes[0].nodeValue;
-
             xmlDoc = xmlDoc.getElementsByTagName("node")[0].childNodes[1];
             xmlDoc = xmlDoc.getElementsByTagName("ThymioBlockly")[0];
             console.log(xmlDoc.getElementsByTagName("xml")[0]);
-
             Blockly.Xml.domToWorkspace(workspace, xmlDoc.getElementsByTagName("xml")[0]);
-
             var fileLoaded = document.getElementById("fileLoaded");
             var showFileName = document.getElementById("showFileName");
-
             fileLoaded.value = fileToLoad.name;
             showFileName.innerHTML = fileToLoad.name;
         };
         fileReader.readAsText(fileToLoad, "UTF-8");
-
         return false;
     };
-
     //to load code and block from a aesl file
     var input = document.getElementById("open-doc");
     input.onchange = function() {
-        var fileToLoad = document.getElementById("open-doc").files[0];
-
-        if (fileToLoad != null) {
-            newDoc();
-            var fileReader = new FileReader();
-            fileReader.onload = function(fileLoadedEvent) {
-                var textFromFileLoaded = fileLoadedEvent.target.result;
-                var parser, xmlDoc
-                if (window.DOMParser) {
-                    parser = new DOMParser();
-                    xmlDoc = parser.parseFromString(textFromFileLoaded, "text/xml");
-                } else {
-
-                    xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-                    xmlDoc.async = false;
-                    xmlDoc.loadXML(textFromFileLoaded);
-                }
-
-                var aeslLoaded = document.getElementById('importExport');
-                aeslLoaded = xmlDoc.getElementsByTagName("node")[0].childNodes[0].nodeValue;
-
-                xmlDoc = xmlDoc.getElementsByTagName("node")[0].childNodes[1];
-
-                xmlDoc = xmlDoc.getElementsByTagName("ThymioBlockly")[0];
-
-
-                //var xml = Blockly.Xml.textToDom(xmlDoc.getElementsByTagName("xml")[0]);
-                Blockly.Xml.domToWorkspace(workspace, xmlDoc.getElementsByTagName("xml")[0]);
-
-                var fileLoaded = document.getElementById("fileLoaded");
-                var showFileName = document.getElementById("showFileName");
-
-                fileLoaded.value = fileToLoad.name;
-                showFileName.innerHTML = fileToLoad.name;
-            };
-            fileReader.readAsText(fileToLoad, "UTF-8");
+            var fileToLoad = document.getElementById("open-doc").files[0];
+            if (fileToLoad != null) {
+                newDoc();
+                var fileReader = new FileReader();
+                fileReader.onload = function(fileLoadedEvent) {
+                    var textFromFileLoaded = fileLoadedEvent.target.result;
+                    var parser, xmlDoc
+                    if (window.DOMParser) {
+                        parser = new DOMParser();
+                        xmlDoc = parser.parseFromString(textFromFileLoaded, "text/xml");
+                    } else {
+                        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+                        xmlDoc.async = false;
+                        xmlDoc.loadXML(textFromFileLoaded);
+                    }
+                    var aeslLoaded = document.getElementById('importExport');
+                    aeslLoaded = xmlDoc.getElementsByTagName("node")[0].childNodes[0].nodeValue;
+                    xmlDoc = xmlDoc.getElementsByTagName("node")[0].childNodes[1];
+                    xmlDoc = xmlDoc.getElementsByTagName("ThymioBlockly")[0];
+                    //var xml = Blockly.Xml.textToDom(xmlDoc.getElementsByTagName("xml")[0]);
+                    Blockly.Xml.domToWorkspace(workspace, xmlDoc.getElementsByTagName("xml")[0]);
+                    var fileLoaded = document.getElementById("fileLoaded");
+                    var showFileName = document.getElementById("showFileName");
+                    fileLoaded.value = fileToLoad.name;
+                    showFileName.innerHTML = fileToLoad.name;
+                };
+                fileReader.readAsText(fileToLoad, "UTF-8");
+            }
         }
-    }
-
-    //to close overlay
+        //to close overlay
     document.getElementById('overlay-msg')
         .addEventListener('click', function(event) {
             document.getElementById("overlay").style.display = "none";
@@ -142,16 +119,11 @@ function start() {
         .addEventListener('click', function(event) {
             document.getElementById("overlay").style.display = "none";
         });
-
     mgr = new goog.net.XhrManager(2, null, 0, 2);
-
-
     //to test if Thymio is connected from the start 
     mgr.send('testCon', 'http://localhost:3000/nodes/', 'GET', null, null, null, function(e) {
-
         var response = this.getResponseText();
         if (response != '' && response != '[]') {
-
             response = response.substring(1, response.length - 1);
             console.log(response);
             response = JSON.parse(response);
@@ -164,16 +136,12 @@ function start() {
             }
         } else {
             connected = false;
-
         }
-
         if (!connected) {
             console.log('not connected');
         }
     });
-
 }
-
 
 function testConnection() {
     console.log('testConnection');
@@ -181,7 +149,6 @@ function testConnection() {
         mgr.abort('testCon');
         mgr.send('testCon', 'http://localhost:3000/nodes/', 'GET', null, null, null, function(e) {
             if (connected) {
-
                 var response = this.getResponseText();
                 if (response != '' && response != '[]') {
                     response = response.substring(1, response.length - 1);
@@ -194,7 +161,6 @@ function testConnection() {
                 } else {
                     connected = false;
                 }
-
                 if (!connected) {
                     console.log('lost connection');
                     document.getElementById("overlay").style.display = "block";
@@ -202,12 +168,9 @@ function testConnection() {
             }
         });
     }, 5000);
-
 }
 
-
 function toCode(lang) {
-
     var output = document.getElementById('importExport');
     output.value = Blockly[lang].workspaceToCode(workspace);
 }
@@ -216,21 +179,13 @@ function updateCode(event) {
     toCode('AESL');
 }
 
-
-
-
-
 function run() {
     toCode('AESL');
-
     var code = document.getElementById('importExport').value.replace(/&/g, '&amp;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
         .replace(/"/g, '&quot;')
         .replace(/'/g, '&apos;');
-
-
-
     var aesl = '<!DOCTYPE aesl-source> \
 		<network> \
 		<keywords flag="true"/> \
@@ -238,20 +193,15 @@ function run() {
         code +
         '</node> \
 		</network>';
-
     var payload = 'file=' + aesl;
-
-
     mgr.abort('testCon');
     mgr.send('testCon', 'http://localhost:3000/nodes/', 'GET', null, null, null, function(e) {
-
         var response = this.getResponseText();
         if (this.getResponseText() != '' && this.getResponseText() != '[]') {
             response = response.substring(1, response.length - 1);
             console.log(response);
             response = JSON.parse(response);
             if (response.connected == 1) {
-
                 console.log("ok");
                 if (!connected) {
                     connected = true;
@@ -265,20 +215,14 @@ function run() {
                 }
             }
         }
-
         if (!connected) {
             console.log("ko");
             document.getElementById("overlay").style.display = "block";
         }
     });
-
-
 }
 
-
-
 function stop() {
-
     var aesl = '<!DOCTYPE aesl-source> \
 		<network> \
 		<keywords flag="true"/> \
@@ -287,15 +231,10 @@ function stop() {
 		motor.right.target = 0 \
 		</node> \
 		</network>';
-
-
-
     var payload = 'file=' + aesl;
     mgr.abort('testCon');
     mgr.send('testCon', 'http://localhost:3000/nodes/', 'GET', null, null, null, function(e) {
-
         var response = this.getResponseText();
-
         if (this.getResponseText() != '' && this.getResponseText() != '[]') {
             response = response.substring(1, response.length - 1);
             console.log(response);
@@ -312,17 +251,14 @@ function stop() {
                 });
             }
         }
-
         if (!connected) {
             console.log("ko");
             document.getElementById("overlay").style.display = "block";
         }
     }, 'GET');
-
 }
 
 function newDoc() {
-
     workspace.clear();
     var input = document.getElementById("open-doc");
     input.value = null;
@@ -332,26 +268,18 @@ function newDoc() {
     showFileName.innerHTML = "";
     var importExport = document.getElementById("importExport");
     importExport.value = "";
-
 }
 
-
-
 function saveFile() {
-
-
     var code = document.getElementById("importExport").value.replace(/&/g, '&amp;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
         .replace(/"/g, '&quot;')
         .replace(/'/g, '&apos;');
-
-
     var xml = Blockly.Xml.workspaceToDom(workspace);
     var textToSave = '<!DOCTYPE aesl-source>\n<network>\n\n<!--list of global events-->\n\n<!--list of constants-->\n\n<!--show keywords state-->\n<keywords flag="true"/>\n\n\<!--node thymio-II-->\n<node nodeId="51938" name="thymio-II">\n\n' +
         code + '\n\n<toolsPlugins>\n<ThymioBlockly>\n' +
         Blockly.Xml.domToPrettyText(xml) + '\n</ThymioBlockly>\n</toolsPlugins>\n</node>\n\n</network>';
-
     var textToSaveAsBlob = new Blob([textToSave], {});
     var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
     var fileNameToSaveAs = document.getElementById("fileLoaded").value;
@@ -359,24 +287,18 @@ function saveFile() {
         fileNameToSaveAs = 'untitled.aesl';
     }
     var downloadLink = document.createElement("a");
-
-
     downloadLink.download = fileNameToSaveAs;
     downloadLink.innerHTML = "Download File";
     downloadLink.href = textToSaveAsURL;
     downloadLink.onclick = destroyClickedElement;
     downloadLink.style.display = "none";
     document.body.appendChild(downloadLink);
-
-
     var isIE = !!navigator.userAgent.match(/Trident/g) || !!navigator.userAgent.match(/MSIE/g);
-
     if (isIE) {
         window.navigator.msSaveOrOpenBlob(textToSaveAsBlob, fileNameToSaveAs);
     } else {
         downloadLink.click();
     }
-
 }
 
 function destroyClickedElement(event) {
