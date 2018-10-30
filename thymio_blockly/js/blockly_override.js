e32da34d689d3ddd4aaa9345e4ac6be3bc99773f
Corentin Jabot <corentin.jabot@mobsya.org>
2018-10-30 13:53:18 +0100

    fix c1ca0d9ce82d09a595f0bfa7eec51b330aa5b686

--- thymio_blockly/js/blockly_override.js
+++ thymio_blockly/js/blockly_override.js
@@ -5,9 +5,9 @@ Blockly.Names.prototype.safeName_ = function(name) {
   if (!name) {
     name = 'unnamed';
   } else {
-   
-	name=name.replace(/ /g, '_').replace(/[^\àâäèéêëîïöôœùûüÿçÀÂÄÈÉÊËÎÏÖÔŒÙÛÜŸÇ_.]+/g, '_');
-	if ('0123456789'.indexOf(name[0]) != -1) {
+
+    name=name.replace(/ /g, '_').replace(/[^\wàâäèéêëîïöôœùûüÿçÀÂÄÈÉÊËÎÏÖÔŒÙÛÜŸÇ_.]+/g, '_');
+    if ('0123456789'.indexOf(name[0]) != -1) {
       name = '_' + name;
     }
     if ('.'.indexOf(name[0]) != -1) {
